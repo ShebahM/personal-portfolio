3dc3fc3529e002ebf224b82a07e3fa358c91c172
Shebah Mimano <85616799+ShebahM@users.noreply.github.com>
2024-11-19 12:25:53 +0000

    changed giithub cards and footer

--- src/components/code-github/code-github.jsx
+++ src/components/code-github/code-github.jsx
@@ -13,21 +13,55 @@ class CodeGithubComponent extends Component {
 
     render() {
         return (
-            <div className="code-github">
-              <p className="headline">Look at my code, my code is amazing</p>
+            <div>
+            <p className="headline">Look at my code, my code is amazing</p>
+                <div className="code-github">
+              
                 <Card>
                   <CardHeader
                     subtitle="Github Repository"
-                    title="salomonelli/personal-portfolio"
+                    title="ShebahM/personal-portfolio"
                     avatar="github.png"
                   />
                 <CardText>
-                        This application is made with React and Material UI. Check out the code on Github.
+                        This application is made with React and Material UI. Check out the code on Github. <br/>
+                        Forked from salomonelli/personal-portfolio. <br/>
+                        Check out the code on Github.
                     </CardText>
                     <CardActions>
-                        <FlatButton label="View on github" href="https://github.com/salomonelli/personal-portfolio" target="_blank"/>
+                        <FlatButton label="View on github" href="https://github.com/ShebahM/personal-portfolio" target="_blank"/>
                     </CardActions>
                 </Card>
+                <Card>
+                  <CardHeader
+                    subtitle="Github Repository"
+                    title="ShebahM/GiftsofGrace-static-website"
+                    avatar="github.png"
+                  />
+                <CardText>
+                        This application is made with JavaScript, HTML and SCSS/CSS. <br/>
+                        Check out the code on Github.
+                    </CardText>
+                    <CardActions>
+                        <FlatButton label="View on github" href="https://github.com/ShebahM/GiftsofGrace-static-website" target="_blank"/>
+                    </CardActions>
+                </Card>
+                <Card>
+                  <CardHeader
+                    subtitle="Gitlab Repository"
+                    title="client-work3/provirtuehealthcare"
+                    avatar="github.png"
+                  />
+                <CardText>
+                        This application is made with NextJS and JavaScript. <br/>
+                        Deployed with Vercel. <br/>
+                        Check out the code on Gitlab.
+                    </CardText>
+                    <CardActions>
+                        <FlatButton label="View on gitlab" href="https://gitlab.com/client-work3/provirtuehealthcare" target="_blank"/>
+                    </CardActions>
+                </Card>
+            </div>
             </div>
         );
     }
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,6 +1,7 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './footer.css';
 import FlatButton from 'material-ui/FlatButton';
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 const scrollTo = require('scroll-to');
 
 class FooterComponent extends Component {
@@ -16,25 +17,21 @@ class FooterComponent extends Component {
     return (
       <div className="footer">
         <div className="link-list">
-          <a target="_blank" href="https://github.com/salomonelli">
-            Github
+          <a target="_blank" href="https://github.com/ShebahM">
+            <FaGithub /> Github
           </a>
-           /
-          <a target="_blank" href="https://twitter.com/salomonelli">
-            Twitter
+          <a target="_blank" class="linkedin" href="https://www.linkedin.com/in/shebah-mimano-72704519b/">
+            <FaLinkedin /> LinkedIn
           </a>
-           /
-          <a target="_blank" href="https://www.xing.com/profile/Sara_Steiert">
-            Xing
-          </a>
-           /
-          <a target="_blank" href="https://www.linkedin.com/in/sara-steiert-b0a631134/">
-            
+          <a target="_blank" class="instagram" href="https://www.instagram.com/queeny_shebah/">
+            <FaInstagram /> Instagram
           </a>
         </div>
         <FlatButton labelStyle={{
           "color": "rgba(0, 0, 0, 0.65)"
-        }} label="Back to top" onClick={this.onScrollToTop}/>
+        }} label="Back to top" onClick={this.onScrollToTop} />
+        <div class="copyright"><small>Site designed by Shebah Mimano.<br/> &#169; Copyright 2024 Shebah Mimano. All Rights Reserved.</small></div>
+
       </div>
     );
   }
